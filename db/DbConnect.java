42cc350b90dbf84a63016dc1b64cbc0f7ad3537e
Pratham <71925400+PrathamCS50@users.noreply.github.com>
2023-12-17 23:45:30 +0530

    Update DbConnect.java

--- db/DbConnect.java
+++ db/DbConnect.java
@@ -18,8 +18,8 @@ public class DbConnect {
 
     static {
         try {
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/spendingdb?useSSL=false&allowPublicKeyRetrieval=true", 
-                    "root", "1234567890");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ **user_database** ?useSSL=false&allowPublicKeyRetrieval=true", 
+                    "**user_root**", "**user_password**");
             st = c.createStatement();
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e);
